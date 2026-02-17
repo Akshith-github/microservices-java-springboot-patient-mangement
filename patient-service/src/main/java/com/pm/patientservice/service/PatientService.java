4f007b7d4ddc0722dc59b383baa682f2c6a6c990
Akshith <akshithnukala9@gmail.com>
2026-02-16 19:49:04 -0500

    15 16) delete patient endpoint and open-api-docs for easy api request from frontend UI building

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -52,4 +52,8 @@ public class PatientService {
 
         return PatientMapper.toDTO(patient);
     }
+
+    public void deletePatient(UUID patientID) {
+        patientRepository.deleteById(patientID);
+    }
 }
